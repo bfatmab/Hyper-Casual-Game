0884ee1dfef734df9835a01e22e349dc77c6e7b7
fatmab0 <fatmabasar2000@gmail.com>
2023-05-30 21:17:52 +0300

    part3
    setting ,shop etc. animations is done

--- Hype_ball_oyun_programlama/Assets/Scripts/UIManagerr.cs
+++ Hype_ball_oyun_programlama/Assets/Scripts/UIManagerr.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +8,60 @@ public class UIManagerr : MonoBehaviour
 {
     public Image whiteEffectImage;
     private int effectControl=0;
+
+    public Animator LayoutAnimator;
+
+
+    public GameObject setting_open;
+    public GameObject setting_close;
+
+    public GameObject sound_on;
+    public GameObject sound_off;
+    public GameObject vibration_on;
+    public GameObject vibration_off;
+    public GameObject iap;
+    public GameObject information;
+
+   public void Setting_Open()
+    {
+        setting_open.SetActive(false);
+        setting_close.SetActive(true);
+        LayoutAnimator.SetTrigger("slide_in");
+    }
+
+    public void Setting_Close()
+    {
+        setting_close.SetActive(false);
+        setting_open.SetActive(true);
+        LayoutAnimator.SetTrigger("slide_out");
+    }
+
+    public void Sound_On()
+    {
+        sound_on.SetActive(false);
+        sound_off.SetActive(true);
+    }
+
+    public void Sound_Off()
+    {
+        sound_on.SetActive(true);
+        sound_off.SetActive(false);
+    }
+
+    public void Vibration_On()
+    {
+        vibration_on.SetActive(false);
+        vibration_off.SetActive(true);
+    }
+    public void Vibration_Off()
+    {
+        vibration_off.SetActive(false);
+        vibration_on.SetActive(true);
+    }
+
+
+
+
     public IEnumerator whiteEffect()
     {
         whiteEffectImage.gameObject.SetActive(true);
